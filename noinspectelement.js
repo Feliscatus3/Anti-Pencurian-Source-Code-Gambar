0b222aa3cf1a21c9ba02ce3a544af4ce9e17ce20
Feliscatus <93772648+Feliscatus3@users.noreply.github.com>
2023-02-21 17:54:46 +0700

    Update noinspectelement.js

--- noinspectelement.js
+++ noinspectelement.js
@@ -3,26 +3,9 @@ document.oncontextmenu = function (e) {
     e.preventDefault()
     return false;
 }
-// disable somme additional key
-document.onkeydown = function (e) {
-    // disable f12
-    if (event.keyCode == 123) {
-        return false;
-    }
-    // disable ctrl+shift+I
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-        return false;
-    }
-    // disable ctrl+shift+C
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-        return false;
-    }
-    // disable ctrl+shift+j
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-        return false;
-    }
-    // disable ctrl+shift+U
-    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-        return false;
-    }
-}
+// or use
+
+//<![CDATA[
+// Anti Inspect Element
+!function t(){try{!function t(n){1===(""+n/n).length&&0!==n||function(){}.constructor("debugger")(),t(++n)}(0)}catch(n){setTimeout(t,5e3)}}();
+//]]>
